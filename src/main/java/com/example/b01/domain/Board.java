24a6b5c142074dc419615c0b7ba06242ff950049
yunhuiGwak <kwakyh17@naver.com>
2023-02-12 21:59:19 +0900

    feat: BaseEntity를 상속하도록 변경, Column길이 설정 - 공통속성 클래스인 BaseEntity를 상속하도록 변경 - 각 칼럼들의 길이와 null허용여부 설정

--- src/main/java/com/example/b01/domain/Board.java
+++ src/main/java/com/example/b01/domain/Board.java
@@ -1,21 +1,21 @@
 package com.example.b01.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity // 데이터에 해당하는 객체
-public class Board {
+public class Board extends BaseEntity{
 
     @Id // 엔티티 객체의 구분
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 데이터베이스에 추가될 때 생성되는 번호 이용 키 생성 전략
     private Long bno;
 
+    @Column(length = 500, nullable = false) // 칼럼의 길이와 null허용여부
     private String title;
 
+    @Column(length = 2000, nullable = false)
     private String content;
 
+    @Column(length = 50, nullable = false)
     private String writer;
 
 }
