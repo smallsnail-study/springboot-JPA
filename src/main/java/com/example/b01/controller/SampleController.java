6d006eb537c867bdf620905ff4a8183c570e481b
yunhuiGwak <kwakyh17@naver.com>
2023-02-12 19:59:57 +0900

    feat: 인라인 처리 Thymeleaf의 인라인 기능(상황에 따라 동일한 데이터를 다르게 출력)을 테스트 다양한 종류의 데이터를 Model로 담아서 전달하는 메소드들을 추가(ex2, SampleDTO)

--- src/main/java/com/example/b01/controller/SampleController.java
+++ src/main/java/com/example/b01/controller/SampleController.java
@@ -6,7 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @Log4j2
@@ -25,5 +29,45 @@ public class SampleController {
         List<String> list = Arrays.asList("AAA", "BBB", "CCC", "DDD");
 
         model.addAttribute("list", list);
+    }
+
+    class SampleDTO {
+        private String p1, p2, p3;
+
+        public String getP1() {
+            return p1;
+        }
+
+        public String getp2() {
+            return p2;
+        }
+
+        public String getP3() {
+            return p3;
+        }
+    }
+
+    @GetMapping("/ex/ex2")
+    public void ex2(Model model) {
+        log.info("ex/ex2..................");
+
+        List<String> strList = IntStream.range(1, 10)
+                .mapToObj(i-> "Data"+i)
+                .collect(Collectors.toList());
+
+        model.addAttribute("list", strList);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("A", "AAAA");
+        map.put("B", "BBBB");
+
+        model.addAttribute("map", map);
+
+        SampleDTO sampleDTO = new SampleDTO();
+        sampleDTO.p1 = "Value -- p1";
+        sampleDTO.p2 = "Value -- p2";
+        sampleDTO.p3 = "Value -- p3";
+
+        model.addAttribute("dto", sampleDTO);
     }
 }
