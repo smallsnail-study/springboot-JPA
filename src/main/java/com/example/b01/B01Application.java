8316996291f6e1cfc63182f3162f94d9761040f3
yunhuiGwak <kwakyh17@naver.com>
2023-02-12 21:56:55 +0900

    feat: @EnableJpaAuditing 추가 설정 @AuditingEntityListener를 활성화 시키기 위한 설정

--- src/main/java/com/example/b01/B01Application.java
+++ src/main/java/com/example/b01/B01Application.java
@@ -2,8 +2,10 @@ package com.example.b01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing  // @AuditingEntityListener를 활성화 시키기 위해 추가
 public class B01Application {
 
     public static void main(String[] args) {
