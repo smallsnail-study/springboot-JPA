51c548b2ff1138df43da9632a23f3d2db92aa9b2
yunhuiGwak <kwakyh17@naver.com>
2023-02-12 22:36:44 +0900

    test: insert 기능 테스트 데이터베이스에 insert를 실행하는 기능 테스트 save()이용

---
+++ src/test/java/com/example/b01/repository/BoardRepositoryTests.java
@@ -0,0 +1,33 @@
+package com.example.b01.repository;
+
+import com.example.b01.domain.Board;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Log4j2
+public class BoardRepositoryTests {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    public void testInsert() {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Board board = Board.builder()
+                    .title("title..." + i)
+                    .content("content..." + i)
+                    .writer("user" + (i % 10))
+                    .build();
+
+            // 데이터베이스에 insert를 실행하는 기능은  JpaRepository의 save()를 통해서 이루어진다.
+            // save()는 현재 영속 컨텍스트 내에 데이터가 존재하는지 찾아보고 해당 엔티티 객체가 없을 때는 insert, 존재할때는 update 자동실핼
+            Board result = boardRepository.save(board);
+            log.info("BNO: " + result.getBno());
+        });
+    }
+}
