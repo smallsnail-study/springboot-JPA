ee506239d0d399daa4a121172845b6ab7754433e
yunhuiGwak <kwakyh17@naver.com>
2023-02-13 13:40:48 +0900

    test: 페이징 처리 기능 테스트 - Pageable이라는 타입의 객체를 구성해서 파라미터로 전달 - PageRequest.of(페이지번호,사이즈,Sort.Direction,속성...) : 페이지번호,사이즈,정렬방향,여러속성 지정가능

--- src/test/java/com/example/b01/repository/BoardRepositoryTests.java
+++ src/test/java/com/example/b01/repository/BoardRepositoryTests.java
@@ -5,7 +5,12 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -64,5 +69,23 @@ public class BoardRepositoryTests {
         Long bno = 1L;
 
         boardRepository.deleteById(bno);
+    }
+
+    @Test
+    public void testPaging() {
+
+        // 1 page order by bno desc
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.findAll(pageable);
+
+        log.info("total count: " + result.getTotalElements());
+        log.info("total pages: " + result.getTotalPages());
+        log.info("page number: " + result.getNumber());
+        log.info("page size: " + result.getSize());
+
+        List<Board> todoList = result.getContent();
+
+        todoList.forEach(board -> log.info(board));
     }
 }
