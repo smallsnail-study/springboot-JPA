4e0a5060249fa4718728796e5c513ed8517c0113
yunhuiGwak <kwakyh17@naver.com>
2023-02-20 23:54:50 +0900

    feat: 검색을 위한 메소드(searchAll) 처리 - 반복문과 제어문을 이용해서 처리 - 검색조건(types)은 제목(t),내용(c),작성자(w)로 구성된다고 가정 - 임시로 return값은 null로 지정

--- src/main/java/com/example/b01/repository/search/BoardSearchImpl.java
+++ src/main/java/com/example/b01/repository/search/BoardSearchImpl.java
@@ -47,4 +47,44 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
 
         return null;
     }
+
+    @Override
+    public Page<Board> searchAll(String[] types, String keyword, Pageable pageable) {
+
+        QBoard board = QBoard.board;
+        JPQLQuery<Board> query = from(board);
+
+        // 검색 조건과 키워드가 있따면
+        if ((types != null && types.length > 0) && keyword != null) {
+
+            BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+            for (String type : types) {
+                switch (type) {
+                    case "t":
+                        booleanBuilder.or(board.title.contains(keyword));
+                        break;
+                    case "c":
+                        booleanBuilder.or(board.content.contains(keyword));
+                        break;
+                    case "w":
+                        booleanBuilder.or(board.writer.contains(keyword));
+                        break;
+                }
+            }
+            query.where(booleanBuilder);
+        }
+
+        // bno > 0
+        query.where(board.bno.gt(0L));
+
+        //paging
+        this.getQuerydsl().applyPagination(pageable, query);
+
+        List<Board> list = query.fetch();
+
+        long count = query.fetchCount();
+
+        return null;
+    }
 }
