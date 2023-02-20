f954299a36c824d550f020a2c707c7af0fd4ae0f
yunhuiGwak <kwakyh17@naver.com>
2023-02-20 23:55:26 +0900

    test: 검색을 위한 메소드(searchAll) test - 임시로 return값은 null로 지정했으므로 결과는 반환되지 않지만 쿼리문 실행 확인

--- src/test/java/com/example/b01/repository/BoardRepositoryTests.java
+++ src/test/java/com/example/b01/repository/BoardRepositoryTests.java
@@ -96,4 +96,16 @@ public class BoardRepositoryTests {
 
         boardRepository.search1(pageable);
     }
+
+    @Test
+    public void testSearchALl() {
+
+        String[] types = {"t", "c", "w"};
+
+        String keyword = "1";
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.searchAll(types, keyword, pageable);
+    }
 }
