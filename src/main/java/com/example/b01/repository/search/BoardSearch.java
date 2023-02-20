4e763de931739d4fb2b32c3880c6aff065521cde
yunhuiGwak <kwakyh17@naver.com>
2023-02-20 23:53:03 +0900

    feat: 검색을 위한 메소드 선언 - 검색조건들(types)과 키워드(keyword) - types는 여러 조건의 조합이 가능하도록 처리

--- src/main/java/com/example/b01/repository/search/BoardSearch.java
+++ src/main/java/com/example/b01/repository/search/BoardSearch.java
@@ -8,4 +8,8 @@ public interface BoardSearch {
     // Querydsl을 이용할 인터페이스 선언
 
     Page<Board> search1(Pageable pageable); // 단순히 페이지 처리 기능만을 선언
+
+    // 검색을 위한 검색조건들(여러조건의 조합이 가능)과 키워드를 처리하는 메소드 선언
+    Page<Board> searchAll(String[] types, String keyword, Pageable pageable);
+
 }
