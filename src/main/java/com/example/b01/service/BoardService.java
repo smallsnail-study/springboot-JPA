8067eadd0418f4329b859dc91a2f09562571163f
yunhuiGwak <kwakyh17@naver.com>
2023-04-19 18:09:14 +0900

    :sparkles: 삭제 기능 추가

--- src/main/java/com/example/b01/service/BoardService.java
+++ src/main/java/com/example/b01/service/BoardService.java
@@ -4,6 +4,7 @@ import com.example.b01.dto.BoardDTO;
 
 public interface BoardService {
 
+    // 등록
     Long register(BoardDTO boardDTO);
 
     // 조회작업을 위한 게시물 번호
@@ -11,4 +12,7 @@ public interface BoardService {
 
     // 수정작업 처리
     void modify(BoardDTO boardDTO);
+
+    // 삭제
+    void remove(Long bno);
 }
