fe7a1e9bc0bb6d741b6d5dca470d784c0dbc8065
yunhuiGwak <kwakyh17@naver.com>
2023-04-19 17:54:09 +0900

    :sparkles: @ToString 추가

--- src/main/java/com/example/b01/domain/Board.java
+++ src/main/java/com/example/b01/domain/Board.java
@@ -1,9 +1,6 @@
 package com.example.b01.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -12,6 +9,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Board extends BaseEntity{
 
     @Id // 엔티티 객체의 구분
