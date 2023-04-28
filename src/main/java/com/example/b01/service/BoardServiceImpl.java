5c9512e2e1df811c12da215a0e6adef4ee0797f8
yunhuiGwak <kwakyh17@naver.com>
2023-04-28 15:33:03 +0900

    :sparkles: 목록/검색 serviceimpl 추가

--- src/main/java/com/example/b01/service/BoardServiceImpl.java
+++ src/main/java/com/example/b01/service/BoardServiceImpl.java
@@ -2,14 +2,20 @@ package com.example.b01.service;
 
 import com.example.b01.domain.Board;
 import com.example.b01.dto.BoardDTO;
+import com.example.b01.dto.PageRequestDTO;
+import com.example.b01.dto.PageResponseDTO;
 import com.example.b01.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -59,5 +65,25 @@ public class BoardServiceImpl implements BoardService {
     @Override
     public void remove(Long bno) {
         boardRepository.deleteById(bno);
+    }
+
+    @Override
+    public PageResponseDTO<BoardDTO> list(PageRequestDTO pageRequestDTO) {
+
+        String[] types = pageRequestDTO.getTypes();
+        String keyword = pageRequestDTO.getKeyword();
+        Pageable pageable = pageRequestDTO.getPageable("bno");
+
+        // Page<Board> 는 List<BoardDTO>로 변환 필요
+        Page<Board> result = boardRepository.searchAll(types, keyword, pageable);
+
+        List<BoardDTO> dtoList = result.getContent().stream()
+                .map(board -> modelMapper.map(board, BoardDTO.class)).collect(Collectors.toList());
+
+        return PageResponseDTO.<BoardDTO>withAll()
+                .pageRequestDTO(pageRequestDTO)
+                .dtoList(dtoList)
+                .total((int) result.getTotalElements())
+                .build();
     }
 }
